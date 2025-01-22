f5df739a30f3f14aad901c4632e9e50885ab6155
Bayode Gabriel <bayogabriel24@gmail.com>
2025-01-22 16:09:36 +0100

    V1.0

--- models/opportunity.js
+++ models/opportunity.js
@@ -1,24 +1,24 @@
 import mongoose from 'mongoose';
 
 const OpportunitySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  type: { type: String, enum: ['internship', 'scholarship', 'job', 'volunteer', 'ambassadorship', 'training'], required: true },
-  institution: { type: String, required: true },
-  position: { type: String, required: true },
+  title: { type: String,  },
+  description: { type: String,  },
+  type: { type: String, enum: ['internship', 'scholarship', 'job', 'volunteer', 'ambassadorship', 'training'],  },
+  institution: { type: String,  },
+  position: { type: String,  },
   dateCreated: { type: Date, default: Date.now },
-  applicationDeadline: { type: Date, required: true },
+  applicationDeadline: { type: Date,  },
   imageUrl: { 
     type: String,
     default: 'https://res.cloudinary.com/dq1uyidfy/image/upload/v1736844758/opp_yby0nw.svg'
   },
-  applyLink: { type: String, required: true },
+  applyLink: { type: String,  },
   status: { 
     type: String, 
     enum: ['pending', 'approved', 'rejected', 'archived', 'expired'], 
     default: 'pending' 
   },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User',  },
 });
 
 // Add a method to check if an opportunity should be archived
